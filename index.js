a941c6f6ee1d31679fd1eb16dd6467cdef3d4fab
Abu Bakkar Siddiq <abu.bakkar@sebpo.com>
2022-05-25 17:53:22 +0600

    add mongodb

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
-const express = require('express');
-const cors = require('cors');
-const port = process.env.PORT || 5000;
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
-require('dotenv').config();
+app.use(express.json());
+require("dotenv").config();
 
-app.listen(port,()=>{
-    console.log('listening to port=>',port);
-})
-app.get('/',(req,res)=>{
-    res.send('server');
-})
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://equip_manufacturer:Jq0ppy2tBe16fi61@cluster0.a3zpv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const useCollection = client
+      .db("equipment_manufacturer")
+      .collection("users");
+
+    app.put("/user/:email", async (req, res) => {});
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("listening to port=>", port);
+});
+app.get("/", (req, res) => {
+  res.send("server");
+});
+
+/**
+ * API Naming Convention
+ * app.get('/products') get all products in this collection
+ * app.get(/product/:id) specific product
+ * app.get('/purchase') purchase one
+ * app.patch('purchase/:id')
+ * app.put('purchase/:id') upsert if have update or create
+ * app.delete('/purchase/:id')
+ */
